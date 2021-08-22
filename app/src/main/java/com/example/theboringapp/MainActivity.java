7fa799749267e69e617106eaaba3a77a085c3b1e
SakshamGurbhele <saksham9013gurbhele@gmail.com>
2021-08-22 23:55:31 +0530

    UI improvement

--- app/src/main/java/com/example/theboringapp/MainActivity.java
+++ app/src/main/java/com/example/theboringapp/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.theboringapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -19,44 +23,65 @@ import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
-    public ImageView imageView;
+    ImageView imageView;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imageView = findViewById(R.id.image);
+        imageView = findViewById(R.id.imageView);
+        textView = findViewById(R.id.textView);
 
-        final TextView textView = (TextView) findViewById(R.id.text);
-// ...
+        meme();
 
-// Instantiate the RequestQueue.
+    }
+    public String share;
+
+    public void meme(){
+        // ...
+        // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
         String url ="https://meme-api.herokuapp.com/gimme";
 
-// Request a string response from the provided URL.
+        // Request a string response from the provided URL.
         JsonObjectRequest JsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
                             String url = response.getString("url");
+                            String info = response.getString("author");
+                            share = response.getString("url");
+                            textView.setText(info);
                             Glide.with(MainActivity.this).load(url).into(imageView);
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
-
-
                     }
-
                 }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
+            @Override
+            public void onErrorResponse(VolleyError error) {
 
-                    }
+            }
         });
         // Add the request to the RequestQueue.
         queue.add(JsonObjectRequest);
 
     }
+
+    public void next(View view) {
+        meme();
+    }
+
+    public void share(View view) {
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, share);
+        sendIntent.setType("text/plain");
+
+        Intent shareIntent = Intent.createChooser(sendIntent, null);
+        startActivity(shareIntent);
+    }
+
 }
