74791219426733152052ef884ecee313e845cd66
SakshamGurbhele <saksham9013gurbhele@gmail.com>
2021-11-10 02:41:11 +0530

    readme added

--- app/src/main/java/com/example/theboringapp/MainActivity.java
+++ app/src/main/java/com/example/theboringapp/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         // ...
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="https://meme-api.herokuapp.com/gimme";
+        String url ="https://api.unsplash.com/photos/?client_id=Gq9Gnlgyi5hDIqdQLR_7yOD8sqZRBS2S5kAbrhOCBnw";
 
         // Request a string response from the provided URL.
         JsonObjectRequest JsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
@@ -51,9 +51,10 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
-                            String url = response.getString("url");
-                            String info = response.getString("author");
-                            share = response.getString("url");
+//                            String url = response.getString("thumb");
+                            String url = "https://images.unsplash.com/photo-1599420186946-7b6fb4e297f0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNjIzMzh8MXwxfGFsbHwxfHx8fHx8Mnx8MTYzMjI4ODcwMw&ixlib=rb-1.2.1&q=80&w=400";
+                            String info = response.getString("alt_description");
+                            share = response.getString("small");
                             textView.setText(info);
                             Glide.with(MainActivity.this).load(url).into(imageView);
                         } catch (JSONException e) {
